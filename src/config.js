763167af00a1b29160a2541f6c80a8ed5f91793b
AziEsq <dlifsics@gmail.com>
2026-01-15 12:11:05 -0600

    Add files via upload
    New Config yay!

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// Airtable Configuration
+export const AIRTABLE_CONFIG = {
+  baseId: 'appJaKQWORmGpA8Kh',
+  personalAccessToken: 'pat03ie7FdOrFByOg.0daffb58032c336577a287f00b8d5cb5e8afe8e93573f608be8fe90acdf5d814', // ⬅️ PASTE YOUR ACTUAL AIRTABLE TOKEN HERE
+  candidatesTableName: 'Candidates',
+  statementsTableName: 'Position Statements'
+};
